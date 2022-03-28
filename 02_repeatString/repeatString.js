c36bd3a74a4a7027967743c9dd51aaf1b1db4d3f
Nico van Drunen <nico.van.drunen@gmail.com>
2022-03-29 00:02:29 +0200

    Added functionaliy to the repeatString functino in repeatString.js, all tests pass.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let returnString = '';
+    if(num >= 0){
+        for (let i = 0; i < num; i++) {
+            returnString += string;
+        }
+    } else {
+        returnString = "ERROR";
+    }
+    return returnString;
 };
 
 // Do not edit below this line
