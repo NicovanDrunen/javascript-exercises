19b6968243bcc1eb2e8798296fce6be4e4e5ed2c
Nico van Drunen <nico.van.drunen@gmail.com>
2022-03-29 01:29:42 +0200

    Added functionality to tempConversion function in tempConversion.js, all tests pass.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(tempInF) {
+  return Math.round(((tempInF - 32) / 1.8) * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(tempInC) {
+  return Math.round(((tempInC * 1.8) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
