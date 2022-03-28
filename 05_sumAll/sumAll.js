39bb08362ec1bae6daa03c98ba1bc2becb032bca
Nico van Drunen <nico.van.drunen@gmail.com>
2022-03-29 01:09:08 +0200

    Added functionality to sumAll function in sumAll.js, all tests pass.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(firstNumber, secondNumber) {
+    if (typeof firstNumber != "number" || typeof secondNumber != "number" || firstNumber < 0 || secondNumber < 0) {
+        return "ERROR";
+    }
+    if (firstNumber > secondNumber) {
+        let temp = firstNumber;
+        firstNumber = secondNumber;
+        secondNumber = temp;
+    }
+    let returnValue = 0;
+    for (let i = firstNumber; i <= secondNumber; i++) {
+        returnValue += i;
+    }
+    return returnValue;
 };
 
 // Do not edit below this line
