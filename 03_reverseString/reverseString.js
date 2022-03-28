763641e710925a4d928da56747aeadfd0b12006f
Nico van Drunen <nico.van.drunen@gmail.com>
2022-03-29 00:12:24 +0200

    Added functionality to the reverseString function in reverseString.js, all tests pass.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const array = string.split("");
+    let reverseString = '';
+    array.forEach(letter => {
+        reverseString = letter + reverseString;
+    });
+    return reverseString;
 };
 
 // Do not edit below this line
