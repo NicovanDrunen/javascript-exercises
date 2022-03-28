0e513f88617f2e4cea419a80f498e5891bcc715d
Nico van Drunen <nico.van.drunen@gmail.com>
2022-03-29 00:57:18 +0200

    Added functionality to the removeFromArray function in remobeFromArray.js, all tests pass.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
 const removeFromArray = function() {
-
+    array = arguments[0];
+    for (let i = 1; i < arguments.length; i++) {
+        if(array.includes(arguments[i])){
+            array.splice(array.findIndex(value => value == arguments[i]), 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
